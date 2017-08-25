565f0cba81a9264d31808fb7e8034c9d492d4983
Nate McGregor <natecmcgregor@gmail.com>
2017-08-25 11:33:29 -0700

    start back and front end js framework for menu

--- js/scripts.js
+++ js/scripts.js
@@ -10,6 +10,22 @@ function pizza(pizzaType,pizzaSize,cheese,sauce){
 
 //front-end
 $(document).ready(function(){
+  var toppings = [];
+  var size = $("#selectPizzaSize").val();
+  var sauce = $("#selectSauce").val();
+  var cheese = $("#selectCheese").val();
+  var topping = $("#selectToppings").val();
+  $("select#selectPizza").change(function() {
+    var pizzaType = $("#selectPizza").val();
+    if(pizzaType ==="Build a peetz"){
+      $("#buildPizzaSection").show();
+      $("#toppingsSection").show();
+    }else{
+      $("#toppingsSection").show();
+    }
+  });
 
-
+  $("form#orderForm").submit(function(event) {
+    event.preventDefault();
+  });
 });
