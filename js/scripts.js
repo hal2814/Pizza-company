7ec360f38694ad8a400079fd8256e78587ac58e3
Nate McGregor <natecmcgregor@gmail.com>
2017-08-25 11:55:28 -0700

    add show/hide features based on selections

--- js/scripts.js
+++ js/scripts.js
@@ -10,19 +10,30 @@ function pizza(pizzaType,pizzaSize,cheese,sauce){
 
 //front-end
 $(document).ready(function(){
+  var kenton = 16;
+  var goose = 15;
+  var pearlDowntown = 14;
+  var build = 13;
   var toppings = [];
-  var size = $("#selectPizzaSize").val();
-  var sauce = $("#selectSauce").val();
-  var cheese = $("#selectCheese").val();
-  var topping = $("#selectToppings").val();
+  var priceTotal = 0;
   $("select#selectPizza").change(function() {
     var pizzaType = $("#selectPizza").val();
+    $("#outputSection").show();
+    $("#sizeSection").show();
+    $("#orderButton").show();
+
     if(pizzaType ==="Build a peetz"){
       $("#buildPizzaSection").show();
       $("#toppingsSection").show();
+      priceTotal += build;
+      $("#priceSection").text("$" + priceTotal);
     }else{
       $("#toppingsSection").show();
     }
+    var size = $("#selectPizzaSize").val();
+    var sauce = $("#selectSauce").val();
+    var cheese = $("#selectCheese").val();
+    var topping = $("#selectToppings").val();
   });
 
   $("form#orderForm").submit(function(event) {
