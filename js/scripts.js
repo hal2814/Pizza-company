77a760eb4ceafb6985b83996b5b0cc8a5a92704d
Nate McGregor <natecmcgregor@gmail.com>
2017-08-25 10:05:06 -0700

    add menu selection html

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+//back-end
+function pizza(pizzaType,pizzaSize,cheese,sauce){
+  this.pizzaType = pizzaType;
+  this.pizzaSize = pizzaSize;
+  this.toppings = [];
+  this.cheese = cheese;
+  this.sauce = sauce;
+};
+
+
+//front-end
+$(document).ready(function(){
+
+
+});
