5e08b49ab1db281c0469bc067c661e7a09b515bb
Nate McGregor <natecmcgregor@gmail.com>
2017-08-25 18:00:52 -0700

    finish menu functionality

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,9 @@
 //back-end
+
+function price(initialTotal,sizeTotal,toppingTotal,sauceTotal,cheeseTotal){
+  $("#priceSection").text("$" + (initialTotal+sizeTotal+toppingTotal+sauceTotal+cheeseTotal));
+}
+
 function check(box){
   if(box===true){
     box = false;
@@ -34,7 +39,7 @@ $(document).ready(function(){
   var kenton = 16;
   var goose = 15;
   var pearlDowntown = 14;
-  var build = 13;
+  var build = 9;
   var toppings = [];
 
   var initialTotal = 0;
@@ -44,9 +49,9 @@ $(document).ready(function(){
   var cheeseTotal = 0;
 
   var pizzaType;
-  var sauce = $("#selectSauce").val();
-  var cheese = $("#selectCheese").val();
-  var topping = $("#selectToppings").val();
+  var sauce;
+  var cheese;
+  var topping;
 
   //pizza type
   $("select#selectPizza").change(function() {
@@ -59,37 +64,37 @@ $(document).ready(function(){
       $("#buildPizzaSection").show();
       $("#toppingsSection").show();
       initialTotal = build;
-      $("#priceSection").text("$" + initialTotal);
+      price(initialTotal,sizeTotal,toppingTotal,sauceTotal,cheeseTotal);
     }else{
       $("#toppingsSection").show();
     }
     if(pizzaType ==="The Kenton"){
       initialTotal = kenton;
-      $("#priceSection").text("$" + initialTotal);
+      price(initialTotal,sizeTotal,toppingTotal,sauceTotal,cheeseTotal);
     }
     if(pizzaType ==="The Goose Hollow"){
       initialTotal = goose;
-      $("#priceSection").text("$" + initialTotal);
+      price(initialTotal,sizeTotal,toppingTotal,sauceTotal,cheeseTotal);
     }
     if(pizzaType ==="The Pearl" || pizzaType ==="The Downtown"){
       initialTotal = pearlDowntown;
-      $("#priceSection").text("$" + initialTotal);
+      price(initialTotal,sizeTotal,toppingTotal,sauceTotal,cheeseTotal);
     }
-    sizeTotal = initialTotal;
+    //sizeTotal = initialTotal;
   });
-  var sizeTotal = initialTotal;
+
   //pizza size
   $("select#selectPizzaSize").change(function() {
     var size = $("#selectPizzaSize").val();
     if(size==="Medium (15 inch pie + $6)"){
-      sizeTotal = initialTotal +6;
-      $("#priceSection").text("$" + sizeTotal);
+      sizeTotal = 6;
+      price(initialTotal,sizeTotal,toppingTotal,sauceTotal,cheeseTotal);
     }else if(size==="Large (18 inch pie + $11)"){
-      sizeTotal =initialTotal +11;
-      $("#priceSection").text("$" + sizeTotal);
-    }else{
-      sizeTotal =initialTotal;
-      $("#priceSection").text("$" + sizeTotal);
+      sizeTotal = 11;
+      price(initialTotal,sizeTotal,toppingTotal,sauceTotal,cheeseTotal);
+    }else if(size==="Small (13 inch pie)"){
+      sizeTotal = 0;
+      price(initialTotal,sizeTotal,toppingTotal,sauceTotal,cheeseTotal);
     }
   });
   //pizza toppings
@@ -97,62 +102,62 @@ $(document).ready(function(){
   $("input#tomatoes").change(function() {
     box1 = getTopping("tomatoes",box1,toppings,toppingTotal);
     toppingTotal = (toppings.length * 2);
-    $("#priceSection").text("$" + (sizeTotal+toppingTotal+sauceTotal));
+    price(initialTotal,sizeTotal,toppingTotal,sauceTotal,cheeseTotal);
   });
   var box2 = true;
   $("input#sundried").change(function() {
     box2 = getTopping("sundried",box2,toppings,toppingTotal);
     toppingTotal = (toppings.length * 2);
-    $("#priceSection").text("$" + (sizeTotal+toppingTotal+sauceTotal));
+    price(initialTotal,sizeTotal,toppingTotal,sauceTotal,cheeseTotal);
 
   });
   var box3 = true;
   $("input#onions").change(function() {
     box3 = getTopping("onions",box3,toppings,toppingTotal);
     toppingTotal = (toppings.length * 2);
-    $("#priceSection").text("$" + (sizeTotal+toppingTotal+sauceTotal));
+    price(initialTotal,sizeTotal,toppingTotal,sauceTotal,cheeseTotal);
 
   });
   var box4 = true;
   $("input#tempeh").change(function() {
     box4 = getTopping("tempeh",box4,toppings,toppingTotal);
     toppingTotal = (toppings.length * 2);
-    $("#priceSection").text("$" + (sizeTotal+toppingTotal+sauceTotal));
+    price(initialTotal,sizeTotal,toppingTotal,sauceTotal,cheeseTotal);
 
   });
   var box5 = true;
   $("input#peppers").change(function() {
     box5 = getTopping("peppers",box5,toppings,toppingTotal);
     toppingTotal = (toppings.length * 2);
-    $("#priceSection").text("$" + (sizeTotal+toppingTotal+sauceTotal));
+    price(initialTotal,sizeTotal,toppingTotal,sauceTotal,cheeseTotal);
 
   });
   var box6 = true;
   $("input#jalapenos").change(function() {
     box6 = getTopping("jalapenos",box6,toppings,toppingTotal);
     toppingTotal = (toppings.length * 2);
-    $("#priceSection").text("$" + (sizeTotal+toppingTotal+sauceTotal));
+    price(initialTotal,sizeTotal,toppingTotal,sauceTotal,cheeseTotal);
 
   });
   var box7 = true;
   $("input#basil").change(function() {
     box7 = getTopping("basil",box7,toppings,toppingTotal);
     toppingTotal = (toppings.length * 2);
-    $("#priceSection").text("$" + (sizeTotal+toppingTotal+sauceTotal));
+    price(initialTotal,sizeTotal,toppingTotal,sauceTotal,cheeseTotal);
 
   });
   var box8 = true;
   $("input#garlic").change(function() {
     box8 = getTopping("garlic",box8,toppings,toppingTotal);
     toppingTotal = (toppings.length * 2);
-    $("#priceSection").text("$" + (sizeTotal+toppingTotal+sauceTotal));
+    price(initialTotal,sizeTotal,toppingTotal,sauceTotal,cheeseTotal);
 
   });
   var box9 = true;
   $("input#tofu").change(function() {
     box9 = getTopping("tofu",box9,toppings,toppingTotal);
     toppingTotal = (toppings.length * 2);
-    $("#priceSection").text("$" + (sizeTotal+toppingTotal+sauceTotal));
+    price(initialTotal,sizeTotal,toppingTotal,sauceTotal,cheeseTotal);
 
   });
 
@@ -163,17 +168,17 @@ $(document).ready(function(){
     }else if(sauce === "Tomatoe red sauce"){
       sauceTotal = 0;
     }
-    $("#priceSection").text("$" + (sizeTotal+toppingTotal+sauceTotal));
+    price(initialTotal,sizeTotal,toppingTotal,sauceTotal,cheeseTotal);
   });
 
   $("select#selectCheese").change(function() {
-    sauce = $("#selectCheese").val();
-    if(sauce === "Pesto sauce (+ $3)"){
-      sauceTotal = 3;
-    }else if(sauce === "Tomatoe red sauce"){
-      sauceTotal = 0;
+    cheese = $("#selectCheese").val();
+    if(cheese === "Follow Your Heart (+ $1)" || cheese === "Diaya (+ $1)"){
+      cheeseTotal = 1;
+    }else if(cheese === "Housemade cashew Mozz"){
+      cheeseTotal = 0;
     }
-    $("#priceSection").text("$" + (sizeTotal+toppingTotal+sauceTotal));
+    price(initialTotal,sizeTotal,toppingTotal,sauceTotal,cheeseTotal);
   });
 
   $("form#orderForm").submit(function(event) {
